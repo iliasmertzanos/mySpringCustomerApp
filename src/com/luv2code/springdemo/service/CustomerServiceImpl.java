494cf9b449f5a473bb5a0b7d890e5bbc1b8e527b
iliasmertzanos <mertzanos@gmail.com>
2019-12-08 10:16:11 +0100

    Delete function added in all layers

--- src/com/luv2code/springdemo/service/CustomerServiceImpl.java
+++ src/com/luv2code/springdemo/service/CustomerServiceImpl.java
@@ -35,6 +35,13 @@ public class CustomerServiceImpl implements CustomerService {
 		
 		return customerDAO.getCustomer(theId);
 	}
+
+	@Override
+	@Transactional
+	public void deleteCustomer(int theId) {
+		
+		customerDAO.deleteCustomer(theId);
+	}
 }
 
 
